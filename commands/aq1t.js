ab3b00c2106550856dd0043b509b7906c28a0103
Jake Kapitz <inbox@jakekapitz.com>
2020-08-20 18:19:55 +1000

    Implemented markdown-table
    Updated commands to use markdown-table package instead of custom commands

--- commands/aq1t.js
+++ commands/aq1t.js
@@ -2,6 +2,7 @@ const {google} = require('googleapis');
 const token = require("../token.json");
 const credentials = require("../credentials.json");
 const sheets = google.sheets('v4');
+const table = require('markdown-table');
 
 module.exports = {
     name: 'aq1t',
@@ -18,35 +19,8 @@ module.exports = {
       };
       try {
         const response = (await sheets.spreadsheets.values.get(request)).data;
-        const formatValues = (values) => {
-          let formattedValues = [];
-          let rowLength = values[0].length + 1;
-          values.forEach(row => {
-            
-            // Create space variable for alignment
-            let space = ' ';
-            
-            // Access 1st item in array (member name), add spaces to 20 characters total, add to the new array
-            let member = row[0];
-            let memberFormatted = `${member}${space.repeat(20 - member.length)}`;
-            member = memberFormatted;
-            formattedValues.push(member);
-            
-            // Access 2nd item in array (path 1), add spaces to 10 characters total, add to the new array
-            let time = row[1];
-            let timeFormatted = `${time}${space.repeat(12 - time.length)}`;
-            time = timeFormatted;
-            formattedValues.push(time);
-            
-            // Add a newline character to the array
-            formattedValues.push("\n");
-          })
-          formattedValues.splice(rowLength, 0, `${'='.repeat(32)}\n`);
-          let msg = `\`\`\`${formattedValues.join('')}\`\`\``;
-          return msg;
-        }
-        const times = formatValues(response.values);
-        message.channel.send(times);
+        const msg = `\`\`\`${table(response.values, {align: ['l', 'c', 'c']})}\`\`\``;
+        message.channel.send(msg);
       } catch (err) {
         console.error(err);
       }
--- commands/aq3p.js
+++ commands/aq3p.js
@@ -2,6 +2,7 @@ const {google} = require('googleapis');
 const token = require("../token.json");
 const credentials = require("../credentials.json");
 const sheets = google.sheets('v4');
+const table = require('markdown-table');
 
 module.exports = {
     name: 'aq3p',
@@ -18,41 +19,8 @@ module.exports = {
       };
       try {
         const response = (await sheets.spreadsheets.values.get(request)).data;
-        const formatValues = (values) => {
-          let formattedValues = [];
-          let rowLength = values[0].length + 1;
-          values.forEach(row => {
-            
-            // Create space variable for alignment
-            let space = ' ';
-            
-            // Access 1st item in array (member name), add spaces to 20 characters total, add to the new array
-            let member = row[0];
-            let memberFormatted = `${member}${space.repeat(20 - member.length)}`;
-            member = memberFormatted;
-            formattedValues.push(member);
-            
-            // Access 2nd item in array (path 1), add spaces to 10 characters total, add to the new array
-            let s1path = row[1];
-            let s1pathFormatted = `${s1path}${space.repeat(10 - s1path.length)}`;
-            s1path = s1pathFormatted;
-            formattedValues.push(s1path);
-            
-            // Access 3rd item in array (path 2), add spaces to 10 characters total, add to the new array
-            let s2path = row[2];
-            let s2pathFormatted = `${s2path}${space.repeat(10 - s2path.length)}`;
-            s2path = s2pathFormatted;
-            formattedValues.push(s2path);
-            
-            // Add a newline character to the array
-            formattedValues.push("\n");
-          })
-          formattedValues.splice(rowLength, 0, `${'='.repeat(40)}\n`);
-          let msg = `\`\`\`${formattedValues.join('')}\`\`\``;
-          return msg;
-        }
-        const paths = formatValues(response.values);
-        message.channel.send(paths);
+        const msg = `\`\`\`${table(response.values, {align: ['l', 'c', 'c']})}\`\`\``;
+        message.channel.send(msg);
       } catch (err) {
         console.error(err);
       }
